04f0ed5bb5e993d23e8a1b436a418c7de9fdcc82
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2026-05-17 03:30:15 +0100

    Update nav links display and bodyScrollUpgrade

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const footer = document.querySelector("#footer");
 const hamMenu = document.querySelector(".topnav__ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 const topNavOverlay = document.querySelector(".topnav__overlay");
+const topNavLinks = document.querySelector(".topnav__links");
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
   topNavOverlay.classList.toggle("active");
-  bodyScrollLockUpgrade.disableBodyScroll(offScreenMenu);
+
+  if (offScreenMenu.classList.contains("active")) {
+    bodyScrollLockUpgrade.disableBodyScroll(offScreenMenu);
+  } else {
+    bodyScrollLockUpgrade.enableBodyScroll(offScreenMenu);
+  }
 });
